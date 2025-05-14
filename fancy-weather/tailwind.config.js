5cac7273949a3527eeb4fb01ee8a7a6ea32346d5
pashaSok <syffiks1998@gmail.com>
2025-05-14 16:46:58 +0300

    feat: create header

---
+++ fancy-weather/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './style.css',
+    './main.js',    
+    './src/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      animation: {
+        spin: 'spin 1s linear infinite',
+      },
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      backgroundBlendMode: {
+        'color-burn': 'color-burn',
+      },
+    },
+  },
+  plugins: [],
+}
+
